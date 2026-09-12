b431f5c4f55492d0734bc068fd634ff860c66a49
Lantyrn <184561204+jc23-7@users.noreply.github.com>
2026-09-12 16:17:07 -0400

    Update buttons in LevelSelect scene to include completed and uncompleted textures.

--- warioware-stardance/Scenes/LevelSelect/constellation_button.gd
+++ warioware-stardance/Scenes/LevelSelect/constellation_button.gd
@@ -2,9 +2,35 @@ extends TextureButton
 
 @export var level_info: Control
 
+@export var click_mask: BitMap
+
+@export_group("Uncompleted Textures")
+@export var uncompleted_normal: Texture2D
+@export var uncompleted_hover: Texture2D
+@export var uncompleted_selected: Texture2D
+
+@export_group("Completed Textures")
+@export var completed_normal: Texture2D
+@export var completed_hover: Texture2D
+@export var completed_selected: Texture2D
+
+
+var completed = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	for constellation in Global.constellations:
+		if constellation.constellation_name == str(name):
+			if constellation.completed:
+				texture_normal = completed_normal
+				texture_hover = completed_hover
+				texture_pressed = completed_selected
+			else:
+				texture_normal = uncompleted_normal
+				texture_hover = uncompleted_hover
+				texture_pressed = uncompleted_selected
+			texture_click_mask = click_mask
+			break
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
