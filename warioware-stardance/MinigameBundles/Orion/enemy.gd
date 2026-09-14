2988f2a9c8a7d3ddfd0b6fc61a551a8e8081e16f
Lantyrn <184561204+jc23-7@users.noreply.github.com>
2026-09-14 17:55:46 -0400

    Add art for heart icons in GameStats.

--- warioware-stardance/MinigameBundles/Orion/enemy.gd
+++ warioware-stardance/MinigameBundles/Orion/enemy.gd
@@ -2,35 +2,44 @@ extends CharacterBody2D
 
 @onready var parent_minigame = $"../"
 @onready var player = $"../Player"
+@onready var animated_sprite = $AnimatedSprite2D
 
 const CHASE_SPEED = 100.0
 const CHARGE_SPEED = 500.0
 
-enum State {CHASE, CHARGE, RECOVER}
+enum State {CHASE, CHARGE, RECOVER, ATTACK}
 var current_state = State.CHASE
 var charge_dir = Vector2.ZERO
 var wait_time := 0.0
 var touching_player = false
 
+func _ready() -> void:
+	animated_sprite.animation_finished.connect(_on_animation_finished)
+
 func _physics_process(delta: float) -> void:
 	if parent_minigame.game_ended:
 		velocity = Vector2.ZERO
 	else:
-		if current_state == State.CHASE:
-			chase()
-		elif current_state == State.CHARGE:
-			if wait_time <= 0:
-				if charge_dir == Vector2.ZERO:
-					charge_dir = position.direction_to(player.position).normalized()
-				charge()
-			else:
-				wait_time -= delta
-				velocity = Vector2.ZERO
-		elif current_state == State.RECOVER:
+		if current_state == State.ATTACK:
 			velocity = Vector2.ZERO
-			wait_time -= delta
-			if wait_time <= 0:
-				current_state = State.CHASE
+		else:
+			animated_sprite.play("default")
+			if current_state == State.CHASE:
+				chase()
+			elif current_state == State.CHARGE:
+				if wait_time <= 0:
+					if charge_dir == Vector2.ZERO:
+						charge_dir = position.direction_to(player.position).normalized()
+					charge()
+				else:
+					wait_time -= delta
+					velocity = Vector2.ZERO
+			elif current_state == State.RECOVER:
+				velocity = Vector2.ZERO
+				wait_time -= delta
+				if wait_time <= 0:
+					current_state = State.CHASE
+			
 
 		move_and_slide()
 		
@@ -38,14 +47,16 @@ func _physics_process(delta: float) -> void:
 			var touched_player = false
 			for i in range(get_slide_collision_count()):
 				var collision = get_slide_collision(i)
+				current_state = State.ATTACK
+				animated_sprite.play("attack")
 				if collision.get_collider() is CharacterBody2D:
 					touched_player = true
-				current_state = State.RECOVER
 				wait_time = 2.0
 			if touching_player and not touched_player:
 				touching_player = false
 			elif touched_player and not touching_player:
-				parent_minigame.lives -= 1
+				if current_state == State.ATTACK:
+					parent_minigame.lives -= 1
 
 func chase() -> void:
 	var direction = position.direction_to(player.position).normalized()
@@ -57,3 +68,8 @@ func chase() -> void:
 	
 func charge() -> void:
 	velocity = charge_dir * CHARGE_SPEED
+	
+func _on_animation_finished() -> void:
+	if animated_sprite.animation == "attack":
+		current_state = State.RECOVER
+		
--- warioware-stardance/MinigameBundles/UrsaMajor/hearts.gd
+++ warioware-stardance/MinigameBundles/UrsaMajor/hearts.gd
@@ -1,20 +1,26 @@
 extends Node2D
 
+@export var heart_template: PackedScene
 @onready var game_stats: Node2D = $"../../"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var num = 0
-	for child in get_children():
-		child.position.x = 25 + 40 * num
-		num += 1
-		child.show()
-
+	await game_stats.ready
+	if game_stats.display_lives:
+		for i in range(game_stats.parent.lives):
+			var new_heart = heart_template.instantiate()
+			add_child(new_heart)
+			new_heart.position.x = 25 + 30 * i
+			new_heart.global_position.y = position.y
+			new_heart.frame = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var num = 0
-	for child in get_children():
-		num += 1
-		if num > game_stats.parent.lives:
-			child.hide()
+	if game_stats.display_lives:
+		var num = 0
+		for child in get_children():
+			num += 1
+			if num > game_stats.parent.lives:
+				child.frame = 1
+			else:
+				child.frame = 0
