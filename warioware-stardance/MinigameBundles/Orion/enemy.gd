07de949283088ab43bc24ba7642238980ff3aa24
Lantyrn <184561204+jc23-7@users.noreply.github.com>
2026-09-23 20:53:43 -0400

    Add art for Orion minigame.

--- warioware-stardance/MinigameBundles/Orion/enemy.gd
+++ warioware-stardance/MinigameBundles/Orion/enemy.gd
@@ -5,16 +5,17 @@ extends CharacterBody2D
 @onready var animated_sprite = $AnimatedSprite2D
 
 const CHASE_SPEED = 100.0
-const CHARGE_SPEED = 500.0
+const CHARGE_SPEED = 450.0
 
 enum State {CHASE, CHARGE, RECOVER, ATTACK}
-var current_state = State.CHASE
+var current_state = State.RECOVER
 var charge_dir = Vector2.ZERO
-var wait_time := 0.0
+var wait_time := 1.0
 var touching_player = false
 
 func _ready() -> void:
 	animated_sprite.animation_finished.connect(_on_animation_finished)
+	look_at(player.position)
 
 func _physics_process(delta: float) -> void:
 	if parent_minigame.game_ended:
@@ -23,10 +24,11 @@ func _physics_process(delta: float) -> void:
 		if current_state == State.ATTACK:
 			velocity = Vector2.ZERO
 		else:
-			animated_sprite.play("default")
 			if current_state == State.CHASE:
+				animated_sprite.play("default")
 				chase()
 			elif current_state == State.CHARGE:
+				animated_sprite.play("default")
 				if wait_time <= 0:
 					if charge_dir == Vector2.ZERO:
 						charge_dir = position.direction_to(player.position).normalized()
@@ -35,12 +37,13 @@ func _physics_process(delta: float) -> void:
 					wait_time -= delta
 					velocity = Vector2.ZERO
 			elif current_state == State.RECOVER:
+				animated_sprite.play("default")
+				animated_sprite.frame = 1
 				velocity = Vector2.ZERO
 				wait_time -= delta
 				if wait_time <= 0:
 					current_state = State.CHASE
 			
-
 		move_and_slide()
 		
 		if current_state == State.CHARGE:
@@ -56,15 +59,18 @@ func _physics_process(delta: float) -> void:
 				touching_player = false
 			elif touched_player and not touching_player:
 				if current_state == State.ATTACK:
-					parent_minigame.lives -= 1
+					player.lose_life()
+					
 
 func chase() -> void:
+	look_at(player.position)
 	var direction = position.direction_to(player.position).normalized()
 	velocity = direction * CHASE_SPEED 
-	if position.distance_to(player.position) < 100.0:
+	if position.distance_to(player.position) < 200.0:
 		charge_dir = Vector2.ZERO
 		current_state = State.CHARGE
 		wait_time = 0.2
+		
 	
 func charge() -> void:
 	velocity = charge_dir * CHARGE_SPEED
--- warioware-stardance/MinigameBundles/Orion/player.gd
+++ warioware-stardance/MinigameBundles/Orion/player.gd
@@ -3,9 +3,11 @@ extends CharacterBody2D
 
 @onready var collision_shape: CollisionShape2D = $"CollisionShape2D"
 @onready var parent_minigame: Node2D = $".."
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 250
+const SPEED = 150
 
+var wait_time = 0.0
 var size
 
 # Called when the node enters the scene tree for the first time.
@@ -17,11 +19,27 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if parent_minigame.game_ended:
 		velocity = Vector2.ZERO
+		animated_sprite.stop()
 	else:
-		var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-		if direction:
-			velocity = direction * SPEED
+		if wait_time <= 0.0:
+			var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+			if direction:
+				velocity = direction * SPEED
+				rotation = direction.angle()
+				animated_sprite.play("walk")
+			else:
+				velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+				animated_sprite.play("default")
+			
+			move_and_slide()
 		else:
-			velocity = velocity.move_toward(Vector2.ZERO, SPEED)
-		
-		move_and_slide()
+			wait_time -= delta
+			animated_sprite.stop()
+
+func lose_life() -> void:
+	parent_minigame.lives -= 1
+	var colour_tween = create_tween()
+	for i in range(3):
+		colour_tween.tween_property(animated_sprite, "modulate", Color(1, 0, 0, 1), 0.1)
+		colour_tween.tween_property(animated_sprite, "modulate", Color(1, 1, 1, 1), 0.1)
+	wait_time = 1.0
